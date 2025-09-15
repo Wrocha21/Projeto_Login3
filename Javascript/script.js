38c17bc59528ddda6687d1b394eacf2f4375d202
Wallaf_Rocha <164103111+Wrocha21@users.noreply.github.com>
2025-09-15 20:57:08 -0300

    reajustandoJ

--- Javascript/script.js
+++ Javascript/script.js
@@ -6,7 +6,18 @@ const senha = document.getElementById('senha')
 const res = document.getElementById('res')
 
 function logar(){
-    return nome.value.length == 0 || email.value.length == 0 || senha.value.length == 0 
-    ? res.innerHTML = 'Os campos são obrigatorios!' 
-    : location.href = 'sitep/index.html'
+    event.preventDefault()
+    if (nome.value.length == 0 ||email.value.length ==0|| senha.value.length == 0) {
+        res.style.color = 'red'
+        res.innerHTML = 'Os campos são obrigatorios!'
+        setTimeout(() =>{
+            res.innerHTML = ''
+        }, 2000)
+    } else {
+        res.style.color = 'green'
+        res.innerHTML = 'Logado com sucesso!'
+        setTimeout(() => {
+            location.href = '../sitep/index.html'
+        }, 2000);
+    }
 }
